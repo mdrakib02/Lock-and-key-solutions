13907642d4b81fcb591fb4dce53ed7b7c6a8c6a5
mdrakib02z <mdrakibahmed515@gmail.comm>
2025-12-09 09:30:43 -0500

    title update

--- components/stepper-form.tsx
+++ components/stepper-form.tsx
@@ -84,7 +84,7 @@ export default function StepperForm() {
 
             <div className="border-b border-gray-200 bg-white pt-8">
                 <div className="max-w-2xl mx-auto px-6 py-6">
-                    <h1 className="text-2xl md:text-4xl font-bold text-[#FF5144] text-center">Rawkeys Locksmith</h1>
+                    <h1 className="text-2xl md:text-4xl font-bold text-[#FF5144] text-center">Lock & Key Solution</h1>
                 </div>
             </div>
 
